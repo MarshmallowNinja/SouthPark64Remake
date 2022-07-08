fc7d7223e0ca07a78f44b2d627a386064e40c916
Marshmallow Ninja <marshmallowninja@disroot.org>
2022-07-08 13:18:13 -0500

    Mark the Farter scene for deletion.

--- Game/Media/Soundtrack/farts/FartScript.gd
+++ Game/Media/Soundtrack/farts/FartScript.gd
@@ -1,14 +1,7 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func Fart():
 	pass
