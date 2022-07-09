cb5ea57443badec57db796579c3acabf1c0e22f6
Marshmallow Ninja <marshmallowninja@disroot.org>
2022-07-09 10:22:24 -0500

    Removed huge skybox for exports sake.

--- Game/Widgets/Main Menu/MainMenuScriptsYo.gd
+++ Game/Widgets/Main Menu/MainMenuScriptsYo.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
-
 func _ready():
+	get_tree().paused = false
 	$Control/Music.play()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	$Control/Menu/Button2.grab_focus()
@@ -9,32 +9,25 @@ func _ready():
 func switchmenus(a, b):
 	get_node(a).set_visible(true)
 	get_node(b).set_visible(false)
-	
-
-# Button Functions switchmenus(visible, hide)
-func _on_Exit_pressed():
-	get_tree().quit()
-
 
 func _on_Button2_pressed():
 	get_tree().change_scene("res://Game/Levels/first.tscn")
 
-
 func _on_settings_pressed():
 	switchmenus("Settings", "Menu")
-
 
 func _on_settomain_pressed():
 	switchmenus("Menu", "Settings")
 
-
 func _on_Button_pressed():
 	switchmenus("Story", "Menu")
-
 
 func _on_backtomain_pressed():
 	switchmenus("Menu", "Story")
 
-
 func _on_New_pressed():
 	get_tree().change_scene("res://Game/Story Mode/Classic/CharacterSelect/StoryCharacterSelect.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
