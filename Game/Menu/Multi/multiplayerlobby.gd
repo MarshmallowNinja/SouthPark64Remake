a64e424fe9fd3b9d221c4ec370cac1cc59e6cdb5
Marshmallow Ninja <marshmallowninja@disroot.org>
2023-07-10 01:33:46 -0500

    Unfinished Multiplayer Part 3

--- Game/Menu/Multi/multiplayerlobby.gd
+++ Game/Menu/Multi/multiplayerlobby.gd
@@ -3,11 +3,16 @@ extends Control
 func _ready() -> void:
 	pass
 
-
-
 func _on_Button_toggled(button_pressed: bool) -> void:
 	match button_pressed:
 		true:
-			$hotbar/Button.text = Network.IPAddress
+			$main/hotbar/Button.text = Network.IPAddress
 		false:
-			$hotbar/Button.text = "Click to Show IP."
+			$main/hotbar/Button.text = "Click to Show IP."
+
+func _on_btm_pressed() -> void:
+	get_tree().change_scene("res://Game/Menu/Main Menu/MainMenu.tscn")
+
+
+func _on_text_text_entered(new_text: String) -> void:
+	$main/logs/text.text = ""
