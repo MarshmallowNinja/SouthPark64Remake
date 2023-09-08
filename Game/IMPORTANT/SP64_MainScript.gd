e454e9d9f731a471abf9aca1c66b0a96ffeec735
Marshmallow Ninja <marshmallowninja@disroot.org>
2023-09-07 23:33:09 -0500

    Small script changes

--- Game/IMPORTANT/SP64_MainScript.gd
+++ Game/IMPORTANT/SP64_MainScript.gd
@@ -8,8 +8,6 @@ func _process(_delta):
 	if Input.is_action_pressed("game_mainmenu"):
 # warning-ignore:return_value_discarded
 		get_tree().change_scene("res://Game/Menu/Main Menu/MainMenu.tscn")
-	if Input.is_action_just_pressed("num_test1"):
-		PlayerController.ChangeCharacter(0, 1)
 
 func SettingsInit():
 	SaveGame.loadconf()
