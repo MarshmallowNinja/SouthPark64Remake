f26c9d22229dd9a0a25b633998ae55af9bf95b70
Marshmallow Ninja <marshmallowninja@disroot.org>
2023-09-17 19:47:58 -0500

    Developer Character select near finalized

--- Game/Menu/DevMenus/CharSelect/DevCharSelect.gd
+++ Game/Menu/DevMenus/CharSelect/DevCharSelect.gd
@@ -5,5 +5,9 @@ func _ready() -> void:
 	$Characters/PopupMenu.popup_centered()
 
 
-func _on_PopupMenu_id_pressed(id: int) -> void:
-	pass # Replace with function body.
+func _on_PopupMenu_id_pressed(id: int):
+	PlayerController.ChangeCharacter(0, id)
+
+
+func _on_respawn_pressed() -> void:
+	$Characters/PopupMenu.popup_centered()
